8db057379033119fe87ece3e31ef9a0bc0534fce
Boddu Venkata Sai Ganesh <40751093+saiganesh1212@users.noreply.github.com>
2020-11-26 16:22:33 +0530

    Deleted Surya

--- Auth_Service_Without_DB/Repository/AuthRepo.cs
+++ Auth_Service_Without_DB/Repository/AuthRepo.cs
@@ -8,7 +8,7 @@ namespace Auth_Service_Without_DB.Repository
 {
     public class AuthRepo : IAuthRepo
     {
-        private static List<User> users=new List<User>() { new User { UserId = 1, Username = "Ganesh", Password = "Sai@1212" }, new User { UserId = 2, Username = "Deepak", Password = "Deep@40" }, new User { UserId = 3, Username = "Nani", Password = "Nan@145" }, new User { UserId = 4, Username = "Suresh", Password = "Sur@34" },new User { UserId = 1, Username = "Surya", Password = "Surya@12" } };
+        private static List<User> users=new List<User>() { new User { UserId = 1, Username = "Ganesh", Password = "Sai@1212" }, new User { UserId = 2, Username = "Deepak", Password = "Deep@40" }, new User { UserId = 3, Username = "Nani", Password = "Nan@145" }, new User { UserId = 4, Username = "Suresh", Password = "Sur@34" } };
         
         public User Login(User user)
         {
